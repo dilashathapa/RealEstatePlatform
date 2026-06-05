cdfb497df5cf4633e6e67a3384d3a49b56bda30c
dilashathapa <dilashathapa15@gmail.com>
2026-06-05 12:49:10 +0545

    user model, registration controller, and email service

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,48 @@
+const sendEmail = async (option) => {
+    try {
+        const BREVO_API_KEY = process.env.BREVO_KEY_API?.trim();
+        if (!BREVO_API_KEY) {
+            console.error("Missing BREVO_API_KEY in the .env file");
+            throw new Error("Missing Email Api Key");
+        }
+
+        const data = {
+            sender: {
+                name: "Real Estate Platform",
+                email: process.env.EMAIL_USER
+            },
+            to: [{email: option.email}],
+            subject: option.subject,
+            htmlContent: options.message
+        };
+
+        const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+            method: "POST",
+            headers: {
+            "api-key": BREVO_API_KEY,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(data),
+    
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+        console.log("Email sent successfully via Brevo:", result.messageId);
+        
+    } else {
+        console.error("Brevo API key Error:", result)
+        throw new Error(result.message || "Couldnot send email via Brevo");
+        
+    }
+        }
+
+        catch (error) {
+           console.error("Brevo Email Error:", result)
+        throw new Error("Couldnot send email via Brevo");
+
+        }
+};
+export default sendEmail;
